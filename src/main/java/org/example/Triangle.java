0747f7b171eb8421ce1fa5900e36adaf3bbe95bb
Pavel Davydov <Davydov.works@gmail.com>
2024-03-12 02:52:25 -0400

    add task

--- src/main/java/org/example/Triangle.java
+++ src/main/java/org/example/Triangle.java
@@ -1,5 +1,17 @@
 package org.example;
 
-public class Triangle implements Figure{
+import java.util.Scanner;
 
+public class Triangle implements Figure{
+    @Override
+    public int getType(){
+        return 3;
+    }
+    @Override
+    public void getSquare(Scanner input){
+        System.out.println("Введите длину основания и высоту к этому основанию");
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        System.out.println( "Площадь треугольника " + a*b/2);
+    }
 }
