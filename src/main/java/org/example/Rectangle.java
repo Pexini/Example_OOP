98b73f935c3b506f426799fff3549e3649d2ad66
Pavel Davydov <Davydov.works@gmail.com>
2024-03-12 02:46:58 -0400

    add task

---
+++ src/main/java/org/example/Rectangle.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Rectangle implements Figure {
+    @Override
+    public int getType() {
+        return 0;
+    }
+
+    @Override
+    public void getSquare(Scanner input) {
+        System.out.println("Введите длины сторон прямогуольника");
+        int a = input.nextInt();
+        int b = input.nextInt();
+        System.out.println("Площадь прямоугольника " + a * b);
+    }
+
+}
---
+++ src/main/java/org/example/Triangle.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class Triangle implements Figure{
+
+}
